663f7e536a9fa73c74f1c5d121676f698c326a64
Abid Hasan <likhon2422@gmail.com>
2026-05-16 15:13:07 +0600

    add filter & map

--- my_app/src/App.jsx
+++ my_app/src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import Card from './components/Card'
-
 import './App.css'
 
 // function App(){
@@ -26,29 +25,126 @@ import './App.css'
 // export default App
 
 
+// function App() {
+//   return (
+//     <div>
+
+//       <Card
+//         name="Abid"
+//         role="Frontend Developer"
+//         avatar="https://i.pravatar.cc/150?img=1"
+//       />
+
+//       <Card
+//         name="Rahim"
+//         role="Backend Developer"
+//         avatar="https://i.pravatar.cc/150?img=2"
+//       />
+
+//       <Card
+//         name="Karim"
+//         role="UI Designer"
+//         avatar="https://i.pravatar.cc/150?img=3"
+//       />
+
+//     </div>
+//   );
+// }
+
+// export default App;
+// const people = [
+//   'Creola Katherine Johnson: mathematician',
+//   'Mario José Molina-Pasquel Henríquez: chemist',
+//   'Mohammad Abdus Salam: physicist',
+//   'Percy Lavon Julian: chemist',
+//   'Subrahmanyan Chandrasekhar: astrophysicist'
+// ];
+
+//  function List() {
+//   const listItems = people.map(person =>
+//     <li>{person}</li>
+//   );
+//   return <ul>{listItems}</ul>;
+// }
+// export default List;
+
+//import  People  from './components/data';
+// function App(){
+//   const listItem = People.map( person => 
+//     <li key={person.id}>
+//       <img src={person.image} alt={person.name} />
+//       <p>
+//         <b>{person.name}</b> {' '+ person.profession + ' '}   known for {person.accomplishment}
+//       </p>
+
+//     </li>
+//   );
+//     return <ul>{listItem}</ul>;
+// }
+// export default App;
+import People from './components/data';
+
 function App() {
+
+  const chemists = People.filter(
+    person => person.profession === 'chemist'
+  );
+
+  const others = People.filter(
+    person => person.profession !== 'chemist'
+  );
+
   return (
-    <div>
 
-      <Card
-        name="Abid"
-        role="Frontend Developer"
-        avatar="https://i.pravatar.cc/150?img=1"
-      />
+    <>
 
-      <Card
-        name="Rahim"
-        role="Backend Developer"
-        avatar="https://i.pravatar.cc/150?img=2"
-      />
+      <h1>Chemists</h1>
 
-      <Card
-        name="Karim"
-        role="UI Designer"
-        avatar="https://i.pravatar.cc/150?img=3"
-      />
+      <ul>
+        {chemists.map(person =>
 
-    </div>
+          <li key={person.id}>
+
+            <img
+              src={person.image}
+              alt={person.name}
+              width="150"
+            />
+
+            <p>
+              <b>{person.name}</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+
+          </li>
+        )}
+      </ul>
+
+      <h1>Everyone Else</h1>
+
+      <ul>
+        {others.map(person =>
+
+          <li key={person.id}>
+
+            <img
+              src={person.image}
+              alt={person.name}
+              width="150"
+            />
+
+            <p>
+              <b>{person.name}</b>
+              {' ' + person.profession + ' '}
+              known for {person.accomplishment}
+            </p>
+
+          </li>
+        )}
+      </ul>
+
+    </>
   );
 }
 
---
+++ my_app/src/components/data.jsx
@@ -0,0 +1,32 @@
+ const People = [{
+  id: 0,
+  name: 'Creola Katherine Johnson',
+  profession: 'mathematician',
+  accomplishment: 'spaceflight calculations',
+  image: "/images/1.png"
+}, {
+  id: 1,
+  name: 'Mario José Molina-Pasquel Henríquez',
+  profession: 'chemist',
+  accomplishment: 'discovery of Arctic ozone hole',
+  image:"/images/2.png"
+}, {
+  id: 2,
+  name: 'Mohammad Abdus Salam',
+  profession: 'physicist',
+  accomplishment: 'electromagnetism theory',
+  image: "/images/3.png"
+}, {
+  id: 3,
+  name: 'Percy Lavon Julian',
+  profession: 'chemist',
+  accomplishment: 'pioneering cortisone drugs, steroids and birth control pills',
+  image: "/images/4.png"
+}, {
+  id: 4,
+  name: 'Subrahmanyan Chandrasekhar',
+  profession: 'astrophysicist',
+  accomplishment: 'white dwarf star mass calculations',
+  image: "/images/5.png"
+}];
+export default People;
